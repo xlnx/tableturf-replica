77f80b8de396ffa46188f41762a4d1be53de19a8
koishi <qwq@koishi.top>
2023-01-10 01:48:50 +0800

    fix chrome svg text not rendered correctly

--- src/engine/Platform.ts
+++ src/engine/Platform.ts
@@ -1,10 +1,19 @@
 import MobileDetect from "mobile-detect";
+import UAParser from "ua-parser-js";
 
 const md = new MobileDetect(window.navigator.userAgent);
 const isMobile = md.mobile() != null;
 
+const parser = new UAParser(window.navigator.userAgent);
+
 export class Platform {
   static get isMobile() {
     return isMobile;
+  }
+
+  // support -webkit-* extensions ?
+  static get isWebKit() {
+    const { name } = parser.getEngine();
+    return name == "Blink" || name == "WebKit";
   }
 }
--- src/ui/components/CardLarge.tsx
+++ src/ui/components/CardLarge.tsx
@@ -6,6 +6,7 @@ import { Card } from "./Card";
 import { SquareTilemap } from "./SquareTilemap";
 import { v4 } from "uuid";
 import { measureTextWidth } from "../../engine/Utils";
+import { Platform } from "../../engine/Platform";
 
 const logger = getLogger("card-large");
 logger.setLevel("info");
@@ -29,7 +30,7 @@ function SvgImage({ ...props }) {
   return <image preserveAspectRatio="none" {...props} />;
 }
 
-const style = {
+const svgStyle = {
   Common: {
     cardBg: "/textures/MngCardBG_Cmn_00.webp",
     InnerFrameBg: ({ ...props }) => <rect fill="#070707" {...props} />,
@@ -66,6 +67,439 @@ const style = {
   },
 };
 
+function renderSvg(card: ICard, player: IPlayerId) {
+  const { cardBg, InnerFrameBg, costImg, NameBg } = svgStyle[card.rarity];
+  const spMeter = Array(10);
+  for (let i = 0; i < card.count.special - 5; ++i) {
+    spMeter[i] = 0;
+  }
+  for (let i = 0; i < Math.min(card.count.special, 5); ++i) {
+    spMeter[i + 5] = 0;
+  }
+  const cardName = I18n.localize(
+    "CommonMsg/MiniGame/MiniGameCardName",
+    card.name
+  );
+  const cardNameScaleX = Math.min(
+    1,
+    layout.width / measureTextWidth(cardName, "40pt Splatoon1")
+  );
+  const id = v4();
+  return (
+    <svg width={layout.width} height={layout.height}>
+      <defs>
+        <mask id={`inner-frame-mask-${id}`}>
+          <rect x={7} y={9} width={330} height={462} fill="white"></rect>
+          <rect
+            x={17}
+            y={18}
+            width={310}
+            height={446}
+            rx={14}
+            fill="black"
+          ></rect>
+        </mask>
+        <filter id={`name-filter-${id}`}>
+          <NameBg
+            x={-100}
+            y={0}
+            width={layout.width * 2}
+            height={layout.height}
+            result="bg"
+          />
+          <feComposite
+            in="SourceGraphic"
+            in2="bg"
+            operator="arithmetic"
+            k1="1"
+            k2="0"
+            k3="0"
+            k4="0"
+          />
+        </filter>
+      </defs>
+      <>
+        <SvgImage
+          className="card-large-card-bg"
+          x={7}
+          y={9}
+          width={330}
+          height={462}
+          xlinkHref={cardBg}
+        />
+        <SvgImage
+          className="card-large-ink"
+          width={layout.width}
+          height={layout.height}
+          xlinkHref="/textures/Ink_03.webp"
+          style={{ filter: "brightness(84.7%)" }}
+        />
+        <SvgImage
+          className="card-large-footer"
+          x={10}
+          y={389}
+          width={324}
+          height={86}
+          xlinkHref="/textures/CardFrame_01.webp"
+          style={{ opacity: 0.9 }}
+        />
+        <InnerFrameBg
+          className="card-large-inner-frame"
+          width={layout.width}
+          height={layout.height}
+          mask={`url(#inner-frame-mask-${id})`}
+        />
+        <SvgImage
+          className="card-large-frame"
+          width={layout.width}
+          height={layout.height}
+          xlinkHref="/textures/CardFrame_00.webp"
+        />
+        <SvgImage
+          className="card-large-bg"
+          width={layout.width}
+          height={layout.height}
+          xlinkHref={`/textures/${card.render.bg}`}
+        />
+        <SvgImage
+          className="card-large-sz-count-base"
+          x={16}
+          y={400}
+          width={64}
+          height={64}
+          xlinkHref={costImg}
+          style={{
+            transformBox: "fill-box",
+            transformOrigin: "center",
+            transform: "rotate(45deg)",
+          }}
+        />
+        <text
+          className="card-large-sz-count"
+          x={48}
+          y={442}
+          textAnchor="middle"
+          fontSize={32}
+          fontFamily="Splatoon1"
+          fill="#efefef"
+          stroke="black"
+          strokeWidth={6}
+          paintOrder="stroke"
+        >
+          {card.count.area}
+        </text>
+        <g
+          className="card-large-sp-meter"
+          style={{ transform: "translate(94px, 422px)" }}
+        >
+          <SquareTilemap
+            id={`card-large-sp-${card.count.special}-${player}`}
+            rect={{
+              size: [5, 2],
+              values: spMeter,
+            }}
+            values={[
+              {
+                image: `/textures/player${player + 1}_special_space.webp`,
+                value: 0,
+              },
+            ]}
+            width={160 / 2}
+            layout={{
+              width: 40,
+              padding: { x: 8, y: 8 },
+            }}
+          />
+        </g>
+        <g
+          width={122}
+          height={122}
+          className="card-large-grid"
+          style={{
+            transformBox: "fill-box",
+            transformOrigin: "center",
+            transform: "translate(200px, 332px) rotate(7deg)",
+          }}
+        >
+          <SquareTilemap
+            id={`card-grid-large-${card.id}-${player}`}
+            rect={card}
+            values={[
+              {
+                image: "/textures/empty_space.webp",
+                alpha: 0.7,
+                value: Spaces.EMPTY,
+              },
+              {
+                image: `/textures/player${player + 1}_trivial_space.webp`,
+                value: Spaces.TRIVIAL,
+              },
+              {
+                image: `/textures/player${player + 1}_special_space.webp`,
+                value: Spaces.SPECIAL,
+              },
+            ]}
+            width={122}
+            layout={{ width: 40 }}
+          />
+        </g>
+      </>
+      <text
+        className="card-large-name"
+        x={"50%"}
+        y={85}
+        textAnchor="middle"
+        fontSize={40}
+        fontFamily="Splatoon1"
+        fill="white"
+        stroke="black"
+        strokeWidth={8}
+        paintOrder="stroke"
+        filter={`url(#name-filter-${id})`}
+        style={{
+          transformBox: "fill-box",
+          transformOrigin: "center",
+          transform: `scaleX(${cardNameScaleX * 100}%)`,
+        }}
+      >
+        {cardName}
+      </text>
+    </svg>
+  );
+}
+
+const webkitStyle = {
+  Common: {
+    cardBg: "/textures/MngCardBG_Cmn_00.webp",
+    innerFrameBg: "linear-gradient(#070707 0 0)",
+    costImg: "/textures/CardCost_00.webp",
+    nameBg: "linear-gradient(#8577ff 0 0)",
+  },
+  Rare: {
+    cardBg: "/textures/MngCardBG_Rre_00.webp",
+    innerFrameBg: "url(/textures/GrdFresh_01.webp)",
+    costImg: "/textures/CardCost_01.webp",
+    nameBg: "url(/textures/GrdFresh_01.webp)",
+  },
+  Fresh: {
+    cardBg: "/textures/MngCardBG_Frh_00.webp",
+    innerFrameBg: "url(/textures/GrdFresh_00.webp)",
+    costImg: "/textures/CardCost_02.webp",
+    nameBg: "url(/textures/GrdFresh_00.webp)",
+  },
+};
+
+function renderWebKit(card: ICard, player: IPlayerId) {
+  const { cardBg, innerFrameBg, costImg, nameBg } = webkitStyle[card.rarity];
+  const spMeter = Array(10);
+  for (let i = 0; i < card.count.special - 5; ++i) {
+    spMeter[i] = 0;
+  }
+  for (let i = 0; i < Math.min(card.count.special, 5); ++i) {
+    spMeter[i + 5] = 0;
+  }
+  const cardName = I18n.localize(
+    "CommonMsg/MiniGame/MiniGameCardName",
+    card.name
+  );
+  const cardNameScaleX = Math.min(
+    1,
+    layout.width / measureTextWidth(cardName, "40pt Splatoon1")
+  );
+  return (
+    <div style={{ width: layout.width, height: layout.height }}>
+      <img
+        className="card-large-card-bg"
+        src={cardBg}
+        style={{
+          position: "absolute",
+          left: 7,
+          top: 9,
+          width: 330,
+          height: 462,
+        }}
+      />
+      <img
+        className="card-large-ink"
+        src={`/textures/Ink_03.webp`}
+        style={{
+          position: "absolute",
+          width: layout.width,
+          height: layout.height,
+          filter: "brightness(84.7%)",
+        }}
+      />
+      <img
+        className="card-large-footer"
+        src={`/textures/CardFrame_01.webp`}
+        style={{
+          position: "absolute",
+          width: 324,
+          height: 86,
+          left: 10,
+          top: 389,
+          opacity: 0.9,
+        }}
+      />
+      <img
+        className="card-large-frame"
+        src={`/textures/CardFrame_00.webp`}
+        style={{
+          position: "absolute",
+          width: layout.width,
+          height: layout.height,
+        }}
+      />
+      <div
+        className="card-large-inner-frame"
+        style={{
+          position: "absolute",
+          left: 10,
+          top: 11,
+          width: 324,
+          height: 460,
+          padding: 4,
+          borderRadius: 16,
+          boxSizing: "border-box",
+          // backgroundClip: "border-box",
+          backgroundImage: innerFrameBg,
+          backgroundSize: "100% 100%",
+          WebkitMask:
+            "linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0)",
+          WebkitMaskComposite: "xor",
+        }}
+      />
+      <img
+        className="card-large-bg"
+        src={`/textures/${card.render.bg}`}
+        style={{
+          position: "absolute",
+          width: layout.width,
+          height: layout.height,
+        }}
+      />
+      <img
+        className="card-large-sz-count-base"
+        src={costImg}
+        style={{
+          position: "absolute",
+          width: 64,
+          height: 64,
+          left: 16,
+          top: 400,
+          transform: "rotate(45deg) ",
+        }}
+      />
+      {[{ WebkitTextStroke: "6px black" }, {}].map((style, i) => (
+        <div
+          className="card-large-sz-count"
+          key={i}
+          style={{
+            position: "absolute",
+            width: 0,
+            height: 0,
+            left: 50,
+            top: 398,
+            display: "flex",
+            justifyContent: "center",
+            whiteSpace: "nowrap",
+            color: "#efefef",
+            fontFamily: "Splatoon1",
+            fontSize: 32,
+          }}
+        >
+          <span style={{ ...style }}>{card.count.area}</span>
+        </div>
+      ))}
+      <div
+        className="card-large-sp-meter"
+        style={{
+          position: "absolute",
+          left: 94,
+          top: 424,
+        }}
+      >
+        <SquareTilemap
+          id={`card-large-sp-${card.count.special}-${player}`}
+          rect={{
+            size: [5, 2],
+            values: spMeter,
+          }}
+          values={[
+            {
+              image: `/textures/player${player + 1}_special_space.webp`,
+              value: 0,
+            },
+          ]}
+          width={160 / 2}
+          layout={{
+            width: 40,
+            padding: { x: 8, y: 8 },
+          }}
+        />
+      </div>
+      <div
+        className="card-large-grid"
+        style={{
+          position: "absolute",
+          left: 200,
+          top: 332,
+          transformOrigin: "center",
+          transform: "rotate(7deg)",
+        }}
+      >
+        <SquareTilemap
+          id={`card-grid-large-${card.id}-${player}`}
+          rect={card}
+          values={[
+            {
+              image: "/textures/empty_space.webp",
+              alpha: 0.7,
+              value: Spaces.EMPTY,
+            },
+            {
+              image: `/textures/player${player + 1}_trivial_space.webp`,
+              value: Spaces.TRIVIAL,
+            },
+            {
+              image: `/textures/player${player + 1}_special_space.webp`,
+              value: Spaces.SPECIAL,
+            },
+          ]}
+          width={122}
+          layout={{ width: 40 }}
+        />
+      </div>
+      {[{ WebkitTextStroke: "8px black" }, {}].map((style, i) => (
+        <div
+          className="card-large-name"
+          key={i}
+          style={{
+            position: "absolute",
+            width: `${100 / cardNameScaleX}%`,
+            height: "100%",
+            left: "50%",
+            transform: `translateX(-50%) scaleX(${cardNameScaleX * 100}%)`,
+            transformOrigin: "center",
+            display: "flex",
+            justifyContent: "center",
+            backgroundClip: "text",
+            WebkitBackgroundClip: "text",
+            backgroundImage: nameBg,
+            backgroundSize: "100% 100%",
+            whiteSpace: "nowrap",
+            color: "transparent",
+            fontFamily: "Splatoon1",
+            fontSize: 40,
+          }}
+        >
+          <span style={{ ...style, paddingTop: 32 }}>{cardName}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 export function CardLarge({
   card: cardId,
   width,
@@ -77,205 +511,22 @@ export function CardLarge({
   const node = useMemo(() => {
     logger.log(`card-large re-render`);
     const card = getCardById(cardId);
-    const { cardBg, InnerFrameBg, costImg, NameBg } = style[card.rarity];
-    const spMeter = Array(10);
-    for (let i = 0; i < card.count.special - 5; ++i) {
-      spMeter[i] = 0;
+    /**
+     * webkit browser supports -webkit-mask & -webkit-text-stroke, which are really helpful
+     * however firefox(Gecko) doesn't support webkit extensions, so we have to switch to
+     * svg implementation. HOWEVER, when blink draws svg <text> with transform properties,
+     * some ghosty glitches occurs from time to time:
+     * https://bugs.chromium.org/p/chromium/issues/detail?id=1270713
+     * the chromium team claimed that they have solved the issue, but not as i'v observed.
+     * besides all these, chrome also performs badly when rendering my svg, at least far
+     * slower than the dom version. the final solution is using ua dependant implementation.
+     */
+    if (Platform.isWebKit) {
+      return renderWebKit(card, player);
+    } else {
+      return renderSvg(card, player);
     }
-    for (let i = 0; i < Math.min(card.count.special, 5); ++i) {
-      spMeter[i + 5] = 0;
-    }
-    const cardName = I18n.localize(
-      "CommonMsg/MiniGame/MiniGameCardName",
-      card.name
-    );
-    const cardNameScaleX = Math.min(
-      1,
-      layout.width / measureTextWidth(cardName, "40pt Splatoon1")
-    );
-    const id = v4();
-    return (
-      <svg width={layout.width} height={layout.height}>
-        <defs>
-          <mask id={`inner-frame-mask-${id}`}>
-            <rect x={7} y={9} width={330} height={462} fill="white"></rect>
-            <rect
-              x={17}
-              y={18}
-              width={310}
-              height={446}
-              rx={14}
-              fill="black"
-            ></rect>
-          </mask>
-          <filter id={`name-filter-${id}`}>
-            <NameBg
-              x={-100}
-              y={0}
-              width={layout.width * 2}
-              height={layout.height}
-              result="bg"
-            />
-            <feComposite
-              in="SourceGraphic"
-              in2="bg"
-              operator="arithmetic"
-              k1="1"
-              k2="0"
-              k3="0"
-              k4="0"
-            />
-          </filter>
-        </defs>
-        <>
-          <SvgImage
-            className="card-large-card-bg"
-            x={7}
-            y={9}
-            width={330}
-            height={462}
-            xlinkHref={cardBg}
-          />
-          <SvgImage
-            className="card-large-ink"
-            width={layout.width}
-            height={layout.height}
-            xlinkHref="/textures/Ink_03.webp"
-            style={{ filter: "brightness(84.7%)" }}
-          />
-          <SvgImage
-            className="card-large-footer"
-            x={10}
-            y={389}
-            width={324}
-            height={86}
-            xlinkHref="/textures/CardFrame_01.webp"
-            style={{ opacity: 0.9 }}
-          />
-          <InnerFrameBg
-            className="card-large-inner-frame"
-            width={layout.width}
-            height={layout.height}
-            mask={`url(#inner-frame-mask-${id})`}
-          />
-          <SvgImage
-            className="card-large-frame"
-            width={layout.width}
-            height={layout.height}
-            xlinkHref="/textures/CardFrame_00.webp"
-          />
-          <SvgImage
-            className="card-large-bg"
-            width={layout.width}
-            height={layout.height}
-            xlinkHref={`/textures/${card.render.bg}`}
-          />
-          <SvgImage
-            className="card-large-sz-count-base"
-            x={16}
-            y={400}
-            width={64}
-            height={64}
-            xlinkHref={costImg}
-            style={{
-              transformBox: "fill-box",
-              transformOrigin: "center",
-              transform: "rotate(45deg)",
-            }}
-          />
-          <text
-            className="card-large-sz-count"
-            x={48}
-            y={442}
-            textAnchor="middle"
-            fontSize={32}
-            fontFamily="Splatoon1"
-            fill="#efefef"
-            stroke="black"
-            strokeWidth={6}
-            paintOrder="stroke"
-          >
-            {card.count.area}
-          </text>
-          <g
-            className="card-large-sp-meter"
-            style={{ transform: "translate(94px, 422px)" }}
-          >
-            <SquareTilemap
-              id={`card-large-sp-${card.count.special}-${player}`}
-              rect={{
-                size: [5, 2],
-                values: spMeter,
-              }}
-              values={[
-                {
-                  image: `/textures/player${player + 1}_special_space.webp`,
-                  value: 0,
-                },
-              ]}
-              width={160 / 2}
-              layout={{
-                width: 40,
-                padding: { x: 8, y: 8 },
-              }}
-            />
-          </g>
-          <g
-            width={122}
-            height={122}
-            className="card-large-grid"
-            style={{
-              transformBox: "fill-box",
-              transformOrigin: "center",
-              transform: "translate(200px, 332px) rotate(7deg)",
-            }}
-          >
-            <SquareTilemap
-              id={`card-grid-large-${card.id}-${player}`}
-              rect={card}
-              values={[
-                {
-                  image: "/textures/empty_space.webp",
-                  alpha: 0.7,
-                  value: Spaces.EMPTY,
-                },
-                {
-                  image: `/textures/player${player + 1}_trivial_space.webp`,
-                  value: Spaces.TRIVIAL,
-                },
-                {
-                  image: `/textures/player${player + 1}_special_space.webp`,
-                  value: Spaces.SPECIAL,
-                },
-              ]}
-              width={122}
-              layout={{ width: 40 }}
-            />
-          </g>
-        </>
-        <text
-          className="card-large-name"
-          x={"50%"}
-          y={85}
-          textAnchor="middle"
-          fontSize={40}
-          fontFamily="Splatoon1"
-          fill="white"
-          stroke="black"
-          strokeWidth={8}
-          paintOrder="stroke"
-          filter={`url(#name-filter-${id})`}
-          style={{
-            transformBox: "fill-box",
-            transformOrigin: "center",
-            transform: `scaleX(${cardNameScaleX * 100}%)`,
-          }}
-        >
-          {cardName}
-        </text>
-      </svg>
-    );
-  }, [cardId, width]);
+  }, [cardId, width, player]);
 
   return (
     <Card
