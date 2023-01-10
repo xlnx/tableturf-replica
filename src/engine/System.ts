98f0e064728fe2a1cb81259f8447e66272511477
koishi <qwq@koishi.top>
2023-01-11 04:00:39 +0800

    fix slow networking may interrupt some display

--- src/engine/System.ts
+++ src/engine/System.ts
@@ -1,5 +1,9 @@
 import { Container, Loader, Texture } from "pixi.js";
 import { Platform } from "./Platform";
+import { getLogger } from "loglevel";
+
+const logger = getLogger("system");
+logger.setLevel("info");
 
 export class PixiRootContainer extends Container {}
 
@@ -8,6 +12,11 @@ export class System extends Platform {
   static readonly args = this.url.searchParams;
 
   static texture(path: string): Texture {
-    return Loader.shared.resources[path].texture;
+    try {
+      return Loader.shared.resources[path].texture;
+    } catch {
+      logger.error(`texture [${path}] not found`);
+      return Texture.WHITE;
+    }
   }
 }
